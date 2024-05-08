817ca34e1783c40b341ea3b8f78f9883a3abe555
Nahuel <nahue_cge@hotmail.com>
2024-05-08 16:06:48 -0300

    s

--- home/pedidos/views.py
+++ home/pedidos/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from clientes.models import Cliente
+from clientes.serializers import ClienteSerializer
 import json
 # Create your views here.
 
 def crearPedido(request):   
     clientes = Cliente.objects.all().filter(estado=True)
-    clientes_serializados = json.dumps(list(clientes.values())) 
-
+    clientes_serializados = ClienteSerializer(clientes, many=True).data
 
     context = {
-        'clientes': clientes_serializados
+        'clientes': json.dumps(clientes_serializados)
     }
     return render(request, 'pedidos/agregarPedido.html', context)
