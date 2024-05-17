79a156a2e91876d374cac71e08a86a37954b94f4
Nahuel <nahue_cge@hotmail.com>
2024-05-17 14:33:52 -0300

    add mostrar cliente

--- home/clientes/urls.py
+++ home/clientes/urls.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-   path('listar/', views.listar_clientes , name="listarClientes")
+   path('listar/', views.listar_clientes , name="listarClientes"),
+   path('listar/<int:id>', views.mostrar_cliente , name="mostrarCliente"),
+   path('buscar/', views.obtener_clientes , name="buscarClientes")
 ]
--- home/clientes/views.py
+++ home/clientes/views.py
@@ -3,21 +3,32 @@ from .models import Cliente
 from .serializers import ClienteSerializer
 from django.http import JsonResponse
 import json
+
 # Create your views here.
 def listar_clientes(request):
     todos_los_clientes = Cliente.objects.all()
     todos_los_clientes_serilizados = ClienteSerializer(todos_los_clientes, many=True).data
 
-
     context = {
         'clientes': todos_los_clientes_serilizados
     }
     return render(request, 'clientes/listarClientes.html', context)
+    
 
+def mostrar_cliente(request,id):
+    cliente_seleccionado = Cliente.objects.get(identificacion=id)
+    cliente_seleccionado_serializado = ClienteSerializer(cliente_seleccionado).data
+
+    context = {
+        'cliente':cliente_seleccionado_serializado
+    }
+
+    return render(request, 'clientes/mostrarCliente.html', context)
 
 def obtener_clientes(request):
     q = request.GET.get('q')  # Obtener el valor de 'q' de los parámetros GET en la URL
     clientes = Cliente.objects.all()
+    print(q)
     
     if q:
         clientes = clientes.filter(nombre__icontains=q) | clientes.filter(identificacion__icontains=q)
