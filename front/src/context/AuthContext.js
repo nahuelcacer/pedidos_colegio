656fed9bc64df3a689e34292ed2640df693a97cc
Nahuel <nahue_cge@hotmail.com>
2024-06-04 15:57:22 -0300

    login an logout

--- front/src/context/AuthContext.js
+++ front/src/context/AuthContext.js
@@ -1,6 +1,6 @@
 import { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import loginFetch from "../service/loginUser";
+import loginFetch from "../service/user";
 
 
 
